f89ccdfe3fdf43ac136d6f5340ce0c949a9ec4a9
GabrielLopesAgiova <glopesagiova@gmail.com>
2022-07-03 20:46:58 -0300

    feat: gravarAgenda and removerCompromisso

--- funcoes/gravarAgenda.py
+++ funcoes/gravarAgenda.py
@@ -1,2 +1,18 @@
 def gravarAgenda(calendario):
-    return
+    nome = input('digite o nome da agenda: ')
+    agenda = open(nome, "w+")
+    embelezador = '=-' * 20
+
+    contador = 0
+    for mes in calendario:
+        for compromisso in mes:
+            contador += 1
+            agenda.write(f"{contador}º compromisso\n")
+            agenda.write(f"""Compromisso no dia: {compromisso['Dia']}
+Horário: {compromisso['Horario']}
+Compromisso: {compromisso['Compromisso']}
+Local: {compromisso['Local']}\n""")
+            if len(compromisso) == 5:
+                agenda.write(f"Observações: {compromisso['Observações']}\n")
+            agenda.write(f'{embelezador} \n')
+    agenda.close()
--- funcoes/removerComprimisso.py
+++ funcoes/removerComprimisso.py
@@ -1,2 +1,26 @@
+from time import sleep
+
+
 def removerCompromisso(calendario):
-    return
+    while True:
+        mes = int(input('Digite o número do mês (ex: 1): '))
+        dia = int(input('Digite o dia: '))
+
+        if len(calendario) > 0:
+            for i in range(len(calendario[mes-1])):
+                values = list(calendario[mes-1][i].values())
+                if values[0] == dia:
+                    while True:
+                        opcao = input(
+                            f'Deseja remover o compromisso: {values[2]} (S/N)? ').upper()
+                        if opcao == 'S':
+                            calendario[mes-1].pop(i)
+                            print('Compromisso removido com successo!')
+                            break
+                        elif opcao == 'N':
+                            break
+        else:
+            print('Nenhum compromisso encontrado!')
+        sleep(2)
+        break
+    return calendario
